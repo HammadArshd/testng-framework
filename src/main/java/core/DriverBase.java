91428a535fea06e1f5fb01aa9ae45f902a3859ec
hammadcrc <hammadcrc@gmail.com>
2018-09-22 23:40:26 +0500

    GoogleTest and GoogleTestWithPriority added

---
+++ src/main/java/core/DriverBase.java
@@ -0,0 +1,22 @@
+package core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverBase {
+
+    protected static WebDriver driver;
+
+    public static void setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://google.com/");
+    }
+}
