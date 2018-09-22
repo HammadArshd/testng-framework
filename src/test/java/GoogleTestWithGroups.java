f0f3dd068042d0cf5965570215f53a5ac35a43da
hammadcrc <hammadcrc@gmail.com>
2018-09-23 00:07:20 +0500

    TestNG Groups introduced

---
+++ src/test/java/GoogleTestWithGroups.java
@@ -0,0 +1,45 @@
+import core.DriverBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTestWithGroups extends DriverBase {
+
+    @BeforeMethod
+    public void setUp(){
+        setUpDriver();
+        System.out.println("@BeforeMethod - setUp()");
+    }
+
+    @Test(priority = 1, groups = "Title")
+    public void googleTitleTest(){
+        Assert.assertEquals(driver.getTitle(), "Google");
+        System.out.println("@Test - googleTitleTest() - priority = 1");
+    }
+
+    @Test(priority = 2, groups = "Logo")
+    public void googleLogoTest(){
+        Assert.assertTrue(driver.findElement(By.xpath("//img[@id='hplogo']")).isDisplayed());
+        System.out.println("@Test - googleLogoTest() - priority = 2");
+    }
+
+    @Test(priority = 3, groups = "Link")
+    public void gmailLinkTest(){
+        Assert.assertTrue(driver.findElement(By.linkText("Gmail")).isDisplayed());
+        System.out.println("@Test - mailLinkTest() - priority = 3");
+    }
+
+    @Test(priority = 4, groups = "Link")
+    public void mailLinkTest(){
+        Assert.assertTrue(driver.findElement(By.linkText("mail")).isDisplayed());
+        System.out.println("@Test - mailLinkTest() - priority = 4");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("@AfterMethod - tearDown()");
+        driver.quit();
+    }
+}
